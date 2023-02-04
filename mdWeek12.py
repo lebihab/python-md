10d1b0a4616e89984f96188bfa35f015d47b2138
lebihab <dmha89@gmail.com>
2023-02-04 18:43:09 +0000

    AWS list

--- mdWeek12.py
+++ mdWeek12.py
@@ -1,1 +1,21 @@
-print("Hello World")
+# Initialize an empty list of AWS services
+aws_services = []
+
+# Add items to the list
+aws_services.append("EC2")
+aws_services.append("S3")
+aws_services.append("Lambda")
+aws_services.append("RDS")
+aws_services.append("SQS")
+
+# Print the list and its length
+print("AWS services:", aws_services)
+print("Length of the list:", len(aws_services))
+
+# Remove two services from the list
+aws_services.remove("RDS")
+aws_services.remove("SQS")
+
+# Print the updated list and its length
+print("Updated AWS services:", aws_services)
+print("New length of the list:", len(aws_services))
